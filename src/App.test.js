2042eea2e986bd7b9ab84436a828faac28afc05f
Saurav Pianist <sauravpunworld2@gmail.com>
2023-09-13 23:11:33 +0530

    modify app.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,7 @@
+import React from 'react';
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import App from './App'; // Adjust the path based on your project structure
+
 
 test('renders learn react link', () => {
   render(<App />);
